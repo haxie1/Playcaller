d42fc34249a134d9a9d2ec2255dffe9208477131
Kam Dahlin <kilodelta@kilodelta-mbp.local>
2016-10-07 15:38:27 -0700

    Added a Settings bar button IBOutlet to the MainViewController along with image art. Once a profile is set, the user will be able to tap the settings button to show the profile screen.

--- Playcaller/MainViewController.swift
+++ Playcaller/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var teamNameLabel: UILabel!
     
+    @IBOutlet weak var settingsBarButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -48,6 +49,8 @@ class MainViewController: UIViewController {
         print("--- call a navigation event for the playbook feature")
     }
     
+    @IBAction func showSettings(_ sender: AnyObject) {
+    }
 
 }
 
